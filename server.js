313b58b40b042a4c488ec9f61a8cacc74a045835
Ryan21b <barronryan65@gmail.com>
2020-06-22 14:28:07 +0200

    Pre-deployment commit

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API..." })
-);
-app.use("/api/users", require("./routes/User"));
+connectDB();
+
+//init Middleware
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/users", require("./routes/Users"));
 app.use("/api/auth", require("./routes/Auth"));
 app.use("/api/contacts", require("./routes/Contacts"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
